8153dde901939bfaadca586ff90d61cbac57a0b8
joaoefornazari <joaoefornazari@gmail.com>
2023-04-13 22:33:20 -0300

    Adding vue bindings to ItemsList and Remove button.

--- src/components/ItemsList.vue
+++ src/components/ItemsList.vue
@@ -1,11 +1,15 @@
 <template>
-	<div v-for="item in items" :key="item.name" class="row my-1">
+	<div v-for="item in cart.items" :key="item.name" class="row my-1">
 		<div class="col">
 			<a :href="item.url">
 				<h5>{{ item.name }}</h5>
 			</a>
 		</div>
-		<div class="cols col-auto"></div>
+		<div class="cols col-auto">
+			<button class="btn btn-danger" @click="cart.removeItem(item)">
+				Remove
+			</button>
+		</div>
 	</div>
 </template>
 
